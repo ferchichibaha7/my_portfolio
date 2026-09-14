a93786bf5487d415b4d25f4198e73b8730f4c551
baha ferchichi <ferchichibaha7@gmail.com>
2026-09-14 13:58:26 +0100

    hello

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "*.{js,ts,jsx,tsx,mdx,mds4}"
+    "*.{js,ts,jsx,tsx,mdx,mds4,baha}"
   ],
   theme: {
   	extend: {
