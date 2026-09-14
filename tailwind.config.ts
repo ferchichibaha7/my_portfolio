fb67405253e057e84b666fb84d2738feb7729652
baha ferchichi <ferchichibaha7@gmail.com>
2026-09-14 14:50:52 +0100

    booring

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "*.{js,ts,jsx,tsx,mdx,mds4,baha}"
+    "*.{js,ts,jsx,tsx,mdx,mds4,baha7}"
   ],
   theme: {
   	extend: {
